2d5c1d2e976a98f342da9c41e2f6f6faa71883f1
VishalPargai <vishal.pargai2017@gmail.com>
2026-05-16 12:02:32 +0530

    lookup override mail

--- server/utils/sendEmail.js
+++ server/utils/sendEmail.js
@@ -11,6 +11,11 @@ const sendEmail = async (options) => {
     throw new Error('Email service not configured. Contact support.');
   }
 
+  // Custom DNS lookup override guaranteeing IPv4 A-record resolution
+  const customLookup = (hostname, opts, callback) => {
+    dns.lookup(hostname, { family: 4 }, callback);
+  };
+
   const transporter = nodemailer.createTransport({
     host: process.env.SMTP_HOST || 'smtp.gmail.com',
     port: Number(process.env.SMTP_PORT) || 587,
@@ -22,6 +27,8 @@ const sendEmail = async (options) => {
     tls: {
       rejectUnauthorized: false,
     },
+    family: 4,
+    lookup: customLookup,
   });
 
   // Verify connection before sending
